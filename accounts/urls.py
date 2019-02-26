16f6b1526850b4019da85e59216412bf6927b99f
wojtekkrupa <43648014+wojtekkrupa@users.noreply.github.com>
2019-02-26 22:34:19 +0100

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views>Signup.as)
+    path('signup/', views.SignUp.as view(), name='SignUp')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.views import generi
+from django.views import generic
 from django.urls import reverse_lazy
 
+<# Create your views here.
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-# Create your views here.
