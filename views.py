5ddc0443de635c7084a6da2d4a7e1dae35dc9e17
wojtekkrupa <43648014+wojtekkrupa@users.noreply.github.com>
2019-02-26 21:16:08 +0100

    Add files via upload
    login req

--- views.py
+++ views.py
@@ -1,2 +1,10 @@
 from django.schortcuts import render
-from django.contrib.auth.decorators
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def panel(request):
+    return render(request, 'panel.html')
